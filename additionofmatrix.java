d04ff68455883d4c94763ad71345ef28dd171c60
Ridafatimah04 <161111031+Ridafatimah04@users.noreply.github.com>
2024-03-01 22:00:36 +0530

    Add files via upload

---
+++ additionofmatrix.java
@@ -0,0 +1,65 @@
+package week7;
+	import java.util.Scanner;
+	public class additionofmatrix<matrix1> {
+		public static void main(String[] args) {
+			int matrix1[][]=new int[3][3];
+			int matrix2[][]=new int[3][3];
+			Scanner sc = new Scanner(System.in);
+			System.out.println("enter matrix_ size");
+			int size=sc.nextInt();
+			System.out.println("enter matrix 1 elements one by one");
+			for(int i=0;i<size;i++) {
+				int[][] matrixl;
+				for(int j=0;j<size;j++)
+					matrixl[i][j]=sc.nextInt();
+				System.out.println("enter matrix2 elements one by one");
+				for(int i1=0;i1<size;i1++)
+					for(int j-0;j<size;j++)
+						matrix2[i1][j]=sc.nextInt();
+			}
+	}
+		int result[][]=new int[3][3];
+		for(inti=0;i<size;i++){
+			for(int j=0;j<size;j++){
+				int i;
+				int[][] matrix1;
+				int[][] matrix2;
+				result[i][j]-matrix1[i][j]+matrix2[i][j]):\;
+			}
+		}
+		System.out.println("Result matrix ");
+		for(inti=0;i<size;i++) {
+			int size;
+			for(int j=0;j<size;j++) {
+				int i;
+				System.out.print(result[i][j]+" ");
+			}
+		System.out.println("");
+		}
+		int result1[][]=new int[3][3];
+		for(inti=0;i<size;i++){
+			int size1;
+			for(int j=0;j<size1;j++){
+				int i;
+				int[][] matrix2;
+				int[][] matrix1;
+				result[i][j]-matrix1[i][j]+matrix2[i][j]):\;
+			}
+		}
+		System.out.println("Result matrix ");
+		for(inti=0;i<size;i++) {
+			int size1;
+			for(int j=0;j<size1;j++) {
+				int i;
+				System.out.print(result[i][j]+" ");
+			}
+		System.out.println("");
+		}
+		private int j(int i) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+	}
+	}
+
+
---
+++ finalise.java
@@ -0,0 +1,34 @@
+package week4;
+	import java.util.Scanner;
+	public class finalise {
+		public static void main(String[] args) {
+			studentl abhinav = new studentl();
+			abhinav.display();
+			System.out.println("Object reference is..abhinav.finalise();"+ abhinav);
+			abhinav.finalise();
+			System.gc();
+			System.out.println("Object garbage collected");
+		}
+	}
+		class studentl{
+			String name;
+			int rno;
+			float res;
+			Scanner sc = new Scanner (System.in);
+		    studentl(){
+		System. out. println ( "Enter Rol1 Number Of Student ");
+		rno = Integer.parseInt (sc.next ());
+		System.out.println("Enter Name Of Student");
+		name = sc.next ();
+		System.out.println("Enter Result of the Student");
+		res = Float.parseFloat(sc.next());
+	 }
+		void display() {
+		System.out.println(rno + "It\t" + name + "It\t" + res);
+		}
+		protected void finalise() {
+		sc.close();
+		System.out.println("Object Cleaned Up By Finalize Method");
+		}
+	}
+
