edc5281049cf6cb506c9d8ad6b3c7c3dbb879b67
Ridafatimah04 <161111031+Ridafatimah04@users.noreply.github.com>
2024-03-14 23:36:25 +0530

    Add files via upload

--- employee.java
+++ employee.java
@@ -1,5 +1,33 @@
 package week2;
-
-public class employee {
-
+class Employee
+{
+	String empName;
+	int empNo;
+	float salary;
+	String empDesig;
+	byte expYrs;
+	
+	void setEmployee(String name,int no,float sal,String desig,byte yrs)
+	{
+		empName=name;empNo=no;salary=sal;empDesig=desig;expYrs=yrs;
+	}
+	void getEmployee()
+	{
+		System.out.println(empName+"\t"+empNo+"\t"+empDesig+"\t"+salary+"\t"+expYrs+"\n");
+	}
 }
+class demoEmployee{
+	public static void main(String args[])
+	{
+		Employee rahul=new Employee();
+		rahul.setEmployee("rahul", 128966, 99999.0f,"PM",(byte)20);
+		System.out.println("empName \t empNo \t empDesig \t salary \t expYrs \n");
+		rahul.getEmployee();
+		Employee ramya=new Employee();
+		ramya.setEmployee("ramya", 7854412, 894576.0f,"Project Leader",(byte)10);
+		ramya.getEmployee();
+	}
+}
+
+
+
