beae77db1739100626e90769ca5d72d864ed7786
Ridafatimah04 <161111031+Ridafatimah04@users.noreply.github.com>
2024-03-14 23:19:02 +0530

    Add files via upload

---
+++ positiveornegative.java
@@ -0,0 +1,22 @@
+package week2;
+public class positiveornegetive {
+	public static void main(String args[])
+	{
+		int num=912;
+		if(num>0)
+		{
+			System.out.println("the given number is positive");
+		}
+		else if(num<0)
+		{
+			System.out.println("the given number is negetive");
+		}
+		else
+		{
+			System.out.println("the given number is zero");
+		}
+	}
+	
+}
+
+
