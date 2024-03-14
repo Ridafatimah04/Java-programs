8ebf270a1d47a30001eeb5da2b96e777698c1778
Ridafatimah04 <161111031+Ridafatimah04@users.noreply.github.com>
2024-03-14 23:56:38 +0530

    Add files via upload

--- cmdline.java
+++ cmdline.java
@@ -1,5 +1,21 @@
 package week2;
-
-public class cmdline {
+public class Cmdline {
+	public static void main(String args[])
+	{
+		if(args.length>0)
+		{
+			System.out.println("java buzzwords...");
+			for (byte i=0;i<args.length;i++)
+			{
+				System.out.println(i+1+"."+ args[i]);
+			}
+		}
+		else
+		{
+			System.out.println("NO COMMAND LINE ARGUMENT FOUND");
+		}
+	}
 
 }
+
+
